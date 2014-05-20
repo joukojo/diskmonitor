443fa35374f28035e097f40aaad3d68fefb5a180
Jouko Johansson <jouko.johansson@sanoma.fi>
2014-05-20 14:51:54 +0300

    added datasource and jdbctemplate with the css/js/img follders

--- src/main/java/org/yogocodes/diskmonitor/config/DiskMonitorConfig.java
+++ src/main/java/org/yogocodes/diskmonitor/config/DiskMonitorConfig.java
@@ -1,10 +1,15 @@
 package org.yogocodes.diskmonitor.config;
 
+import javax.sql.DataSource;
+
+import org.apache.commons.dbcp.BasicDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.view.JstlView;
 import org.springframework.web.servlet.view.UrlBasedViewResolver;
 
@@ -21,6 +26,31 @@ public class DiskMonitorConfig {
 		resolver.setSuffix(".jsp");
 		resolver.setViewClass(JstlView.class);
 		return resolver;
+	}
+
+	@Bean(destroyMethod = "close")
+	public DataSource setupDataSource() {
+		final BasicDataSource dataSource = new BasicDataSource();
+		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+		dataSource.setUrl("jdbc:mysql://127.0.0.1:3306/diskmonitor");
+		dataSource.setUsername("diskuser");
+		dataSource.setPassword("diskmon");
+		dataSource.setMaxActive(50);
+		dataSource.setInitialSize(15);
+		return dataSource;
+	}
+
+	@Bean
+	public JdbcTemplate createJdbcTemplate() {
+		return new JdbcTemplate(setupDataSource());
+	}
+
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/assets/**").addResourceLocations(
+				"classpath:/assets/");
+		registry.addResourceHandler("/css/**").addResourceLocations("/css/");
+		registry.addResourceHandler("/img/**").addResourceLocations("/img/");
+		registry.addResourceHandler("/js/**").addResourceLocations("/js/");
 	}
 
 }
